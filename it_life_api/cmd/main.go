fa81953824cc49483be4999cb54478f0744743a7
hatano-viviane <rikuhatano09@gmail.com>
2022-06-23 00:01:30 +0900

    feat: Add initial endpoint.

---
+++ it_life_api/cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+
+	// Set CORS config.
+	engine.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:3000",
+		},
+		AllowMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		MaxAge: 24 * time.Hour,
+	}))
+
+	engine.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "Hello IT Life",
+		})
+	})
+
+	engine.Run(":8000")
+}
